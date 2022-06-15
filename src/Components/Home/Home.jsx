f4f0410db94f7930754d719e36be609a58a403ce
Kirwa <kkirwa230@gmail.com>
2022-06-15 10:18:24 +0300

    finalized the NavBar.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,36 +4,32 @@ import Grid from '@mui/material/Grid';
 
 import { MiddleRow } from '../MiddleRow/MiddleRow';
 
-import './home.css'
-
+import './home.css';
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Grid container spacing={1} >
-
-
+    <div className="home">
+      <Grid container spacing={1}>
         <Grid item xs={2} sm={2}>
           <div className="lefBarContainer">
             <Leftbar></Leftbar>
           </div>
-          
         </Grid>
 
-        <Grid item xs={9} sm={9}>
-
+        <Grid item xs={6} sm={6}>
           <div className="middleRow">
             <MiddleRow />
           </div>
         </Grid>
 
-       
-        
+        <Grid item xs={3} sm={3}>
+          <div>
+            <Form />
+          </div>
+        </Grid>
       </Grid>
-      
-
     </div>
-  )
+  );
 };
 
 export default Home;
--- src/Components/LeftBar/LeftBar.jsx
+++ src/Components/LeftBar/LeftBar.jsx
@@ -3,9 +3,7 @@ import Container from '@mui/material/Container';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './styles.css'
-
-
+import './styles.css';
 
 import {
   Bookmark,
@@ -18,51 +16,45 @@ import {
   TabletMac,
 } from '@mui/icons-material';
 
-
 const Leftbar = () => {
-  
   return (
-
-    <Container className='mainContainer'>
-
-      <div className='mainDivItem'>
+    <Container className="mainContainer">
+      <div className="mainDivItem">
         <Link to="/">
-          <Home className='icon' />
+          <Home className="icon" />
         </Link>
-        <Typography className='text'>Homepage</Typography>
+        <Typography className="text">Homepage</Typography>
       </div>
-      <div className='mainDivItem'>
-        <Person className='icon' />
-        <Typography className='text'>My Profile</Typography>
-        </div>
-        <div className='mainDivItem'>
-        <Bookmark className='icon' />
-        <Typography className='text'>My Tickets</Typography>
+      <div className="mainDivItem">
+        <Person className="icon" />
+        <Typography className="text">My Profile</Typography>
       </div>
-      <div className='mainDivItem'>
-        <List className='icon' />
-        <Typography className='text'>Categories</Typography>
-      </div>      
-      <div className='mainDivItem'>
-        <TabletMac className='icon' />
-        <Typography className='text'>Apps</Typography>
+      <div className="mainDivItem">
+        <Bookmark className="icon" />
+        <Typography className="text">My Tickets</Typography>
       </div>
-      
-      <div className='mainDivItem'>
-        <Storefront className='icon' />
-        <Typography className='text'>Market Place</Typography>
+      <div className="mainDivItem">
+        <List className="icon" />
+        <Typography className="text">Categories</Typography>
       </div>
-      <div className='mainDivItem'>
-        <Settings className='icon' />
-        <Typography className='text'>Acc.Settings</Typography>
+      <div className="mainDivItem">
+        <TabletMac className="icon" />
+        <Typography className="text">Apps</Typography>
       </div>
 
-      <div className='mainDivItem'>
-        <ExitToApp className='icon' />
-        <Typography className='text'>Logout</Typography>
-        </div>
-      
+      <div className="mainDivItem">
+        <Storefront className="icon" />
+        <Typography className="text">Market Place</Typography>
+      </div>
+      <div className="mainDivItem">
+        <Settings className="icon" />
+        <Typography className="text">Acc.Settings</Typography>
+      </div>
 
+      <div className="mainDivItem">
+        <ExitToApp className="icon" />
+        <Typography className="text">Logout</Typography>
+      </div>
     </Container>
   );
 };
